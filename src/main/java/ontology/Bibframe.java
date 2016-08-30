1940fa2f92d362d6a4e66262d7881dfbeaa854a5
Rurik Greenall <rurik.greenall@gmail.com>
2016-08-31 00:26:37 +0200

    Initial project commit

---
+++ src/main/java/ontology/Bibframe.java
@@ -0,0 +1,234 @@
+package ontology;
+
+import org.apache.jena.rdf.model.Property;
+import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.rdf.model.ResourceFactory;
+
+/**
+ * Responsibility: provides the base ontology.
+ */
+public class Bibframe {
+    Bibframe() {}
+
+    private static final String URI = "http://bibframe.org/vocab/";
+
+    public static String getURI() {
+        return URI;
+    }
+
+    private static Resource resource(String local) {
+        return ResourceFactory.createResource(URI + local);
+    }
+
+    private static Property property(String local) {
+        return ResourceFactory.createProperty(URI + local);
+    }
+
+    public static final Resource AdminMetadata = resource("AdminMetadata");
+    public static final Resource Agent = resource("Agent");
+    public static final Resource AppliedMaterial = resource("AppliedMaterial");
+    public static final Resource Arrangement = resource("Arrangement");
+    public static final Resource AspectRatio = resource("AspectRatio");
+    public static final Resource BaseMaterial = resource("BaseMaterial");
+    public static final Resource BookFormat = resource("BookFormat");
+    public static final Resource Capture = resource("Capture");
+    public static final Resource Carrier = resource("Carrier");
+    public static final Resource Cartographic = resource("Cartographic");
+    public static final Resource Classification = resource("Classification");
+    public static final Resource Collection = resource("Collection");
+    public static final Resource ColorContent = resource("ColorContent");
+    public static final Resource Content = resource("Content");
+    public static final Resource Contribution = resource("Contribution");
+    public static final Resource CopyrightRegistration = resource("CopyrightRegistration");
+    public static final Resource CoverArt = resource("CoverArt");
+    public static final Resource DigitalCharacteristic = resource("DigitalCharacteristic");
+    public static final Resource Dissertation = resource("Dissertation");
+    public static final Resource Emulsion = resource("Emulsion");
+    public static final Resource EnumerationAndChronology = resource("EnumerationAndChronology");
+    public static final Resource Event = resource("Event");
+    public static final Resource Extent = resource("Extent");
+    public static final Resource FontSize = resource("FontSize");
+    public static final Resource Frequency = resource("Frequency");
+    public static final Resource Generation = resource("Generation");
+    public static final Resource GenreForm = resource("GenreForm");
+    public static final Resource Identifier = resource("Identifier");
+    public static final Resource Illustration = resource("Illustration");
+    public static final Resource Instance = resource("Instance");
+    public static final Resource IntendedAudience = resource("IntendedAudience");
+    public static final Resource Issuance = resource("Issuance");
+    public static final Resource Item = resource("Item");
+    public static final Resource Language = resource("Language");
+    public static final Resource Layout = resource("Layout");
+    public static final Resource Media = resource("Media");
+    public static final Resource Mount = resource("Mount");
+    public static final Resource MusicEnsemble = resource("MusicEnsemble");
+    public static final Resource MusicFormat = resource("MusicFormat");
+    public static final Resource MusicInstrument = resource("MusicInstrument");
+    public static final Resource MusicMedium = resource("MusicMedium");
+    public static final Resource MusicVoice = resource("MusicVoice");
+    public static final Resource Notation = resource("Notation");
+    public static final Resource Note = resource("Note");
+    public static final Resource Place = resource("Place");
+    public static final Resource Polarity = resource("Polarity");
+    public static final Resource ProductionMethod = resource("ProductionMethod");
+    public static final Resource ProjectionCharacteristic = resource("ProjectionCharacteristic");
+    public static final Resource ProvisionActivity = resource("ProvisionActivity");
+    public static final Resource ReductionRatio = resource("ReductionRatio");
+    public static final Resource Review = resource("Review");
+    public static final Resource SoundCharacteristic = resource("SoundCharacteristic");
+    public static final Resource SoundContent = resource("SoundContent");
+    public static final Resource Source = resource("Source");
+    public static final Resource Status = resource("Status");
+    public static final Resource Sublocation = resource("Sublocation");
+    public static final Resource Summary = resource("Summary");
+    public static final Resource SupplementaryContent = resource("SupplementaryContent");
+    public static final Resource TableOfContents = resource("TableOfContents");
+    public static final Resource Temporal = resource("Temporal");
+    public static final Resource Title = resource("Title");
+    public static final Resource Topic = resource("Topic");
+    public static final Resource Unit = resource("Unit");
+    public static final Resource UsageAndAccessPolicy = resource("UsageAndAccessPolicy");
+    public static final Resource VideoCharacteristic = resource("VideoCharacteristic");
+    public static final Resource Work = resource("Work");
+    
+    public static final Property acquisitionSource = property("acquisitionSource");
+    public static final Property acquisitionTerms = property("acquisitionTerms");
+    public static final Property adminMetadata = property("adminMetadata");
+    public static final Property agent = property("agent");
+    public static final Property appliedMaterial = property("appliedMaterial");
+    public static final Property arrangement = property("arrangement");
+    public static final Property ascensionAndDeclination = property("ascensionAndDeclination");
+    public static final Property aspectRatio = property("aspectRatio");
+    public static final Property assigner = property("assigner");
+    public static final Property awards = property("awards");
+    public static final Property barcode = property("barcode");
+    public static final Property baseMaterial = property("baseMaterial");
+    public static final Property bookFormat = property("bookFormat");
+    public static final Property capture = property("capture");
+    public static final Property carrier = property("carrier");
+    public static final Property cartographicAttributes = property("cartographicAttributes");
+    public static final Property classification = property("classification");
+    public static final Property classificationPortion = property("classificationPortion");
+    public static final Property code = property("code");
+    public static final Property colorContent = property("colorContent");
+    public static final Property content = property("content");
+    public static final Property contentAccessibility = property("contentAccessibility");
+    public static final Property contribution = property("contribution");
+    public static final Property contributor = property("contributor");
+    public static final Property coordinates = property("coordinates");
+    public static final Property copyrightRegistration = property("copyrightRegistration");
+    public static final Property count = property("count");
+    public static final Property coverArt = property("coverArt");
+    public static final Property credits = property("credits");
+    public static final Property custodialHistory = property("custodialHistory");
+    public static final Property date = property("date");
+    public static final Property degree = property("degree");
+    public static final Property derivedFrom = property("derivedFrom");
+    public static final Property descriptionAuthentication = property("descriptionAuthentication");
+    public static final Property descriptionConventions = property("descriptionConventions");
+    public static final Property descriptionLanguage = property("descriptionLanguage");
+    public static final Property descriptionModifier = property("descriptionModifier");
+    public static final Property digitalCharacteristic = property("digitalCharacteristic");
+    public static final Property dimensions = property("dimensions");
+    public static final Property dissertation = property("dissertation");
+    public static final Property duration = property("duration");
+    public static final Property edition = property("edition");
+    public static final Property editionEnumeration = property("editionEnumeration");
+    public static final Property editionStatement = property("editionStatement");
+    public static final Property electronicLocator = property("electronicLocator");
+    public static final Property emulsion = property("emulsion");
+    public static final Property ensemble = property("ensemble");
+    public static final Property ensembleType = property("ensembleType");
+    public static final Property enumerationAndChronology = property("enumerationAndChronology");
+    public static final Property equinox = property("equinox");
+    public static final Property exclusionGRing = property("exclusionGRing");
+    public static final Property extent = property("extent");
+    public static final Property firstIssue = property("firstIssue");
+    public static final Property fontSize = property("fontSize");
+    public static final Property frequency = property("frequency");
+    public static final Property generation = property("generation");
+    public static final Property generationProcess = property("generationProcess");
+    public static final Property genreForm = property("genreForm");
+    public static final Property geographicCoverage = property("geographicCoverage");
+    public static final Property grantingInstitution = property("grantingInstitution");
+    public static final Property heldBy = property("heldBy");
+    public static final Property hierarchicalLevel = property("hierarchicalLevel");
+    public static final Property historyOfWork = property("historyOfWork");
+    public static final Property identifiedBy = property("identifiedBy");
+    public static final Property illustrativeContent = property("illustrativeContent");
+    public static final Property immediateAcquisition = property("immediateAcquisition");
+    public static final Property instrument = property("instrument");
+    public static final Property instrumentalType = property("instrumentalType");
+    public static final Property intendedAudience = property("intendedAudience");
+    public static final Property issuance = property("issuance");
+    public static final Property itemPortion = property("itemPortion");
+    public static final Property language = property("language");
+    public static final Property lastIssue = property("lastIssue");
+    public static final Property layout = property("layout");
+    public static final Property mainTitle = property("mainTitle");
+    public static final Property media = property("media");
+    public static final Property mount = property("mount");
+    public static final Property musicFormat = property("musicFormat");
+    public static final Property musicKey = property("musicKey");
+    public static final Property musicMedium = property("musicMedium");
+    public static final Property musicOpusNumber = property("musicOpusNumber");
+    public static final Property musicSerialNumber = property("musicSerialNumber");
+    public static final Property musicThematicNumber = property("musicThematicNumber");
+    public static final Property natureOfContent = property("natureOfContent");
+    public static final Property notation = property("notation");
+    public static final Property note = property("note");
+    public static final Property noteType = property("noteType");
+    public static final Property organization = property("organization");
+    public static final Property outerGRing = property("outerGRing");
+    public static final Property part = property("part");
+    public static final Property partName = property("partName");
+    public static final Property partNumber = property("partNumber");
+    public static final Property pattern = property("pattern");
+    public static final Property physicalLocation = property("physicalLocation");
+    public static final Property place = property("place");
+    public static final Property polarity = property("polarity");
+    public static final Property preferredCitation = property("preferredCitation");
+    public static final Property productionMethod = property("productionMethod");
+    public static final Property projection = property("projection");
+    public static final Property projectionChracteristic = property("projectionChracteristic");
+    public static final Property provisionActivity = property("provisionActivity");
+    public static final Property provisionActivityStatement = property("provisionActivityStatement");
+    public static final Property qualifier = property("qualifier");
+    public static final Property reductionRatio = property("reductionRatio");
+    public static final Property relatedTo = property("relatedTo");
+    public static final Property responsibilityStatement = property("responsibilityStatement");
+    public static final Property review = property("review");
+    public static final Property role = property("role");
+    public static final Property scale = property("scale");
+    public static final Property schedulePart = property("schedulePart");
+    public static final Property seriesEnumeration = property("seriesEnumeration");
+    public static final Property seriesStatement = property("seriesStatement");
+    public static final Property shelfMark = property("shelfMark");
+    public static final Property soundCharacteristic = property("soundCharacteristic");
+    public static final Property soundContent = property("soundContent");
+    public static final Property source = property("source");
+    public static final Property spanEnd = property("spanEnd");
+    public static final Property status = property("status");
+    public static final Property subject = property("subject");
+    public static final Property subLocation = property("subLocation");
+    public static final Property subseriesEnumeration = property("subseriesEnumeration");
+    public static final Property subseriesStatement = property("subseriesStatement");
+    public static final Property subtitle = property("subtitle");
+    public static final Property summary = property("summary");
+    public static final Property supplementaryContent = property("supplementaryContent");
+    public static final Property systemRequirements = property("systemRequirements");
+    public static final Property table = property("table");
+    public static final Property tableOfContents = property("tableOfContents");
+    public static final Property tableSeq = property("tableSeq");
+    public static final Property temporalCoverage = property("temporalCoverage");
+    public static final Property title = property("title");
+    public static final Property unit = property("unit");
+    public static final Property usageAndAccessPolicy = property("usageAndAccessPolicy");
+    public static final Property variantType = property("variantType");
+    public static final Property version = property("version");
+    public static final Property videoCharacteristic = property("videoCharacteristic");
+    public static final Property voice = property("voice");
+    public static final Property voiceType = property("voiceType");
+
+
+}
